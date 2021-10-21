01e866a5a5adbbee314c2f03784b0b77a59c889e
Rodrigo Reis <rodgamecodebr@gmail.com>
2021-10-21 13:31:52 -0300

    Added Quaternion class to codebase

---
+++ include/Math/rfQuaternion.h
@@ -0,0 +1,60 @@
+//---------------------------------------------------------------------------//
+//                                                                           //
+// File : rfQuaternion.h													 //
+//                                                                           //
+// Desc : Handle operations regarding orientation                            //
+//															                 //
+//---------------------------------------------------------------------------//
+//     Rain Forest 2021.	Rodrigo dos Reis (reisro)			             //
+//---------------------------------------------------------------------------//
+
+#pragma once
+
+#ifndef _RFQUATERNION_H_
+#define _RFQUATERNION_H_
+
+//-----------------------------------------------------------------------------
+// rfQuaternion Header Includes
+//-----------------------------------------------------------------------------
+#include <rfBaseTypes.h>
+#include <iostream>
+#include <math.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <d3d9.h>
+#include <d3dx9.h>
+#undef WIN32_LEAN_AND_MEAN
+
+//-----------------------------------------------------------------------------
+// Forward Declarations
+//-----------------------------------------------------------------------------
+class rfVector3;
+class rfMatrix;
+
+//-----------------------------------------------------------------------------
+// Main class declarations
+//-----------------------------------------------------------------------------
+class rfQuaternion
+{
+public:
+
+	rfQuaternion();
+	rfQuaternion(rfFloat _x, rfFloat _y, rfFloat _z, rfFloat _w);
+	~rfQuaternion();
+
+	//-------------------------------------------------------------------------
+	// Public Methods
+	//-------------------------------------------------------------------------
+
+	inline static rfQuaternion* RotationYawPitchRoll(rfQuaternion& out, rfFloat yaw, rfFloat pitch, rfFloat roll)
+	{
+		return (rfQuaternion*)D3DXQuaternionRotationYawPitchRoll((D3DXQUATERNION*)&out, yaw, pitch, roll);
+	}
+
+	//-------------------------------------------------------------------------
+	// Public Members
+	//-------------------------------------------------------------------------
+	rfFloat x, y, z, w;
+};
+
+#endif _RFQUATERNION_H_
---
+++ source/Math/rfQuaternion.cpp
@@ -0,0 +1,13 @@
+#include "..\..\include\Math\rfQuaternion.h"
+
+rfQuaternion::rfQuaternion()
+{
+}
+
+rfQuaternion::rfQuaternion(rfFloat x, rfFloat y, rfFloat z, rfFloat w)
+{
+}
+
+rfQuaternion::~rfQuaternion()
+{
+}
